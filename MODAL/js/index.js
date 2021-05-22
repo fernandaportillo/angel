9b009a209c6891d9913ca2ae42024897a83b5957
maria fernanda <fernandaportillo13@gmail.com>
2021-05-22 02:26:56 -0300

    listo

---
+++ MODAL/js/index.js
@@ -0,0 +1,22 @@
+const openModal = document.querySelectorAll("[data-open]");
+const closeModal = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openModal) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeModal) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
